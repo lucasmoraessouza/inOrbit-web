b16ce7d92b85b46733138c426a031ba1754dbfca
lucasmoraessouza <lucasmoraes_souza@outlook.com>
2025-02-09 01:02:52 -0300

    :sparkles: feat: create connection w/ backend and get data

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,20 @@ import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
 import { EmptyGoal } from "./components/empty-goal";
 import { Summary } from "./components/summary";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "./http/get-summary";
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ["summary"],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 segundos
+  });
+
   return (
     <Dialog>
-      {/* <EmptyGoal /> */}
-      <Summary />
-      {/* <CreateGoal /> */}
+      {data && data?.total > 0 ? <Summary /> : <EmptyGoal />}
+      <CreateGoal />
     </Dialog>
   );
 }
