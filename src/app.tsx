29ae8678a55ff9a93f66e6fd394739123b2457ce
lucasmoraessouza <lucasmoraes_souza@outlook.com>
2025-02-08 21:59:35 -0300

    :sparkles: create inital config and pages

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import { Dialog } from "./components/ui/dialog";
+import { CreateGoal } from "./components/create-goal";
+import { EmptyGoal } from "./components/empty-goal";
+import { Summary } from "./components/summary";
+
+export function App() {
+  return (
+    <Dialog>
+      {/* <EmptyGoal /> */}
+      <Summary />
+      {/* <CreateGoal /> */}
+    </Dialog>
+  );
+}
